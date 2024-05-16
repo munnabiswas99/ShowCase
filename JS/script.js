492607227ac61454678277691256414d6d3843bc
munnabiswas99 <biswas15-5261@diu.edu.bd>
2024-05-16 19:26:34 +0600

    Profile page created

--- JS/script.js
+++ JS/script.js
@@ -6,15 +6,51 @@ function community(){
     window.location.href = "Community.html";
 };
 
-function buttonAction1(){
-    document.getElementById('btAction1').style.backgroundColor = '#00A86B';
-    document.getElementById('btAction1').innerText = 'Added'
+function home(){
+    window.location.href = "profile.html"
 }
-function buttonAction2(){
-    document.getElementById('btAction2').style.backgroundColor = '#00A86B';
-    document.getElementById('btAction2').innerText = 'Added'
+
+// function buttonAction1(){
+//     document.getElementById('btAction1').style.backgroundColor = '#00A86B';
+//     document.getElementById('btAction1').innerText = 'Added'
+// }
+// function buttonAction2(){
+//     document.getElementById('btAction2').style.backgroundColor = '#00A86B';
+//     document.getElementById('btAction2').innerText = 'Added'
+// }
+// function buttonAction3(){
+//     document.getElementById('btAction3').style.backgroundColor = '#00A86B';
+//     document.getElementById('btAction3').innerText = 'Added'
+// }
+
+function changeColor1() {
+    const buttons = document.querySelectorAll(".btn-1");
+  
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener("click", function () {
+        buttons[i].style.backgroundColor = "green";
+        buttons[i].innerText = 'Added'
+      });
+    }
 }
-function buttonAction3(){
-    document.getElementById('btAction3').style.backgroundColor = '#00A86B';
-    document.getElementById('btAction3').innerText = 'Added'
+
+function changeColor2() {
+    const buttons = document.querySelectorAll(".btn-2");
+  
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener("click", function () {
+        buttons[i].style.backgroundColor = "gray";
+        buttons[i].innerText = 'Joined'
+      });
+    }
+}
+function changeColor3() {
+    const buttons = document.querySelectorAll(".btn-3");
+  
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener("click", function () {
+        buttons[i].style.backgroundColor = "gray";
+        buttons[i].innerText = 'Following'
+      });
+    }
 }
