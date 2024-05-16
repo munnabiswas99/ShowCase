f6187b470e5c51a819351f7010dbc3b127d0cc89
munnabiswas99 <biswas15-5261@diu.edu.bd>
2024-05-16 15:33:33 +0600

    JS added

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+function homePage(){
+        window.location.href = "home.html";
+    };
+
+function community(){
+    window.location.href = "Community.html";
+};
+
+function buttonAction1(){
+    document.getElementById('btAction1').style.backgroundColor = '#00A86B';
+    document.getElementById('btAction1').innerText = 'Added'
+}
+function buttonAction2(){
+    document.getElementById('btAction2').style.backgroundColor = '#00A86B';
+    document.getElementById('btAction2').innerText = 'Added'
+}
+function buttonAction3(){
+    document.getElementById('btAction3').style.backgroundColor = '#00A86B';
+    document.getElementById('btAction3').innerText = 'Added'
+}
